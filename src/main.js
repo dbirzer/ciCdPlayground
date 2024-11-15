b631e75eed3ba909cc4547e211f47a15b9302c39
dbirzer <johndohsken@outlook.com>
2024-11-15 10:50:34 +0100

    Introduce birzerd

--- src/main.js
+++ src/main.js
@@ -4,27 +4,23 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'Christof Happ',
+		name: 'Dietmar Birzer',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Red tiles on a screen on the wall',
-			'Scripts in strange programming languages',
-			'Some annoying technology preventing me to finish my ticket and breaking production'
+			'Pipelines - Pipelines everywhere'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: ':sadpanda:',
+		tools: 'Jenkins & Gitlab CI',
 		// What do you want to learn in this workshop? 
 		expectations: [
 			'Share knowledge about the basics of CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'reading books', 
-			'cooking', 
-			'playing computer games'
+			'Speed Jigsaw Puzzling'
 		]
 	}
 });
